fa2eb54ca908654607e35c909755c1edba0737ab
sivelin.odegard <sivelin.odegard@students.fhnw.ch>
2020-02-26 12:13:57 +0100

    test

---
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RoutePlannerLib;
+
+namespace RoutePlannerConsole
+{
+    class RoutePlannerConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Route Planner (Version {0})", typeof(RoutePlannerConsoleApp).Assembly.GetName().Version);
+
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312); 
+            Console.WriteLine($"{wayPoint.Name}: {wayPoint.Latitude}/{wayPoint.Longitude}");
+            Console.WriteLine("Git test");
+        }
+    }
+}
---
+++ RoutePlannerLib/RoutePlannerLib.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RoutePlannerLib
+{
+    public class WayPoint 
+    { 
+        public string Name { get; set; } 
+        public double Longitude { get; set; } 
+        public double Latitude { get; set; } 
+        public WayPoint(string name, double latitude, double longitude)
+        { 
+            Name = name; 
+            Latitude = latitude; 
+            Longitude = longitude; 
+        } 
+    }
+}
