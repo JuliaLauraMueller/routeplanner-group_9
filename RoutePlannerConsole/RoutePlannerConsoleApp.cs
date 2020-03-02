b50797940fab4ddb0e28ffb519691182a6de9e2a
Julia Müller <julia.mueller@students.fhnw.ch>
2020-03-02 11:28:15 +0100

    solved task 3 a-d

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -13,10 +13,16 @@ namespace RoutePlannerConsole
             var wayPoint2 = new WayPoint(null, 47.479319847061966, 8.212966918945312);
             var bern = new WayPoint("Bern", 46.947998, 7.448148);
             var tripolis = new WayPoint("Tripolis", 32.876175, 13.187507);
+            var cities = new Cities();
+            var city1 = new City("Bern", "Schweiz", 12345, 46.947998, 7.448148);
             // Console.WriteLine($"{wayPoint.Name}: {wayPoint.Latitude}/{wayPoint.Longitude}");
+
             Console.WriteLine(wayPoint.ToString());
             Console.WriteLine(wayPoint2.ToString());
             Console.WriteLine($"Der Abstand zwischen {bern.Name} und {tripolis.Name} ist {tripolis.Distance(bern):F2} km");
+            //Console.WriteLine(cities.ReadCities("./citiesTestDataLab2.txt"));
+            Console.WriteLine(cities.AddCity(city1));
+            Console.WriteLine(cities.AddCity(city1));
         }
     }
 }
--- RoutePlannerLib/RoutePlannerLib.cs
+++ RoutePlannerLib/RoutePlannerLib.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace RoutePlannerLib
 {
@@ -23,9 +25,101 @@ namespace RoutePlannerLib
         {
             double grad;
             int r = 6371;
-            grad = r * Math.Acos(Math.Sin(this.Latitude) * Math.Sin(target.Latitude)
-                + Math.Cos(this.Latitude) * Math.Cos(target.Latitude) * Math.Cos(this.Longitude - target.Longitude));
-            return grad * (Math.PI / 180);
+            grad = r * Math.Acos(Math.Sin(this.Latitude * (Math.PI / 180)) * Math.Sin(target.Latitude * (Math.PI / 180))
+                + Math.Cos(this.Latitude * (Math.PI / 180)) * Math.Cos(target.Latitude * (Math.PI / 180))
+                * Math.Cos(this.Longitude * (Math.PI / 180) - target.Longitude * (Math.PI / 180)));
+            return grad;
         }
     }
+
+    public class City
+    {
+        public string Name;
+        public string Country;
+        public double Population;
+        public WayPoint Location;
+
+        public City(string name, string country, double population, double latitude, double longitude)
+        {
+            Name = name;
+            Country = country;
+            Population = population;
+            Location = new WayPoint(name, latitude, longitude);
+
+        }
+    }
+
+    public class Cities
+    {
+        private List<City> cityList = new List<City>();
+        public int Count { get { return this.cityList.Count; } }
+
+        public City this[int index] // indexer implementation
+        {
+            
+            get {
+                if (index > cityList.Count)
+                {
+                    throw new ArgumentOutOfRangeException("Index ist zu gross");
+                }
+                else if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Index darf nicht negativ sein");
+                }
+                return this.cityList[index];
+            }
+            set {
+                if (index >= cityList.Count)
+                {
+                    throw new ArgumentOutOfRangeException("Index ist zu gross");
+                }
+                else if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Index darf nicht negativ sein");
+                }
+                cityList[index] = value;
+            }
+
+        }
+
+        public int ReadCities(string filename){
+
+            int counter = 0;
+
+            using (var reader = new StreamReader(filename))
+            {
+                string cityProperty;
+                string cityName;
+                string countryName;
+                double cityPopulation;
+                double cityLatitude;
+                double cityLongitute;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    cityProperty = reader.ReadLine();
+                    cityName = cityProperty.Split("/t").ToString();
+                    countryName = cityProperty.Split("/t").ToString();
+                    // Convert funktioniert noch nicht richtig, Wieso?
+                    cityPopulation = Convert.ToDouble(cityProperty.Split("/t").ToString());
+                    cityLatitude = Convert.ToDouble(cityProperty.Split("/t").ToString());
+                    cityLongitute = Convert.ToDouble(cityProperty.Split("/t").ToString());
+
+                    cityList.Add(new City(cityName, countryName, cityPopulation, cityLatitude, cityLongitute));
+                    Console.WriteLine(cityList);
+                    
+                    Console.WriteLine(reader.ReadLine());
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public int AddCity(City city)
+        {
+            cityList.Add(city);
+            return cityList.Count;
+        }
+    }
+
 }
