48c38467d2299eedc35a02a9653deaabfb218311
Julia Müller <julia.mueller@students.fhnw.ch>
2020-03-02 09:58:56 +0100

    solved task 1 and 2 from Lab02

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -9,9 +9,14 @@ namespace RoutePlannerConsole
         {
             Console.WriteLine("Welcome to Route Planner (Version {0})", typeof(RoutePlannerConsoleApp).Assembly.GetName().Version);
 
-            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312); 
-            Console.WriteLine($"{wayPoint.Name}: {wayPoint.Latitude}/{wayPoint.Longitude}");
-            
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            var wayPoint2 = new WayPoint(null, 47.479319847061966, 8.212966918945312);
+            var bern = new WayPoint("Bern", 46.947998, 7.448148);
+            var tripolis = new WayPoint("Tripolis", 32.876175, 13.187507);
+            // Console.WriteLine($"{wayPoint.Name}: {wayPoint.Latitude}/{wayPoint.Longitude}");
+            Console.WriteLine(wayPoint.ToString());
+            Console.WriteLine(wayPoint2.ToString());
+            Console.WriteLine($"Der Abstand zwischen {bern.Name} und {tripolis.Name} ist {tripolis.Distance(bern):F2} km");
         }
     }
 }
--- RoutePlannerLib/RoutePlannerLib.cs
+++ RoutePlannerLib/RoutePlannerLib.cs
@@ -12,6 +12,20 @@ namespace RoutePlannerLib
             Name = name; 
             Latitude = latitude; 
             Longitude = longitude; 
-        } 
+        }
+
+        public override string ToString()
+        {
+            return $"Waypoint: {this.Name} {this.Latitude:F2} / {this.Longitude:F2}";
+        }
+
+        public double Distance(WayPoint target)
+        {
+            double grad;
+            int r = 6371;
+            grad = r * Math.Acos(Math.Sin(this.Latitude) * Math.Sin(target.Latitude)
+                + Math.Cos(this.Latitude) * Math.Cos(target.Latitude) * Math.Cos(this.Longitude - target.Longitude));
+            return grad * (Math.PI / 180);
+        }
     }
 }
