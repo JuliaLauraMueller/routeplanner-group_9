c54503bd4d5cbc7521f3ad79f5a62f9354e3a122
Julia Müller <julia.mueller@students.fhnw.ch>
2020-03-16 13:01:21 +0100

    added Test-files

--- RoutePlannerTests/WayPointTest_Lab4.cs
+++ RoutePlannerTests/WayPointTest_Lab4.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
-using Fhnw.Ecnf.RoutPlanner.RoutePlannerLib;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerTest
 {
