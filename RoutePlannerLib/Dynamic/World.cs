a51699e13b72fb2e0171394c57344a439a6cdf66
Julia Müller <julia.mueller@students.fhnw.ch>
2020-05-19 13:38:28 +0200

    created dynamic class world

---
+++ RoutePlannerLib/Dynamic/World.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Dynamic;
+using RoutePlannerLib;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Dynamic
+{
+    public class World : DynamicObject
+    {
+        private Cities cities;
+
+        public World(Cities cities)
+        {
+            this.cities = cities;
+
+        }
+
+        public dynamic CityName(City c)
+        {
+            if (cities.FindCity(c.Name))
+            {
+                return c;
+            }
+            else
+                return $"The city {c} does not exist";
+        }
+    }
+}
