b63dea71db1bf697d699e17184ec8609b6b5c7c3
sivelin.odegard <sivelin.odegard@students.fhnw.ch>
2020-05-29 15:46:06 +0200

     Writer

--- RoutePlannerLib/Util/SimpleObjectWriter.cs
+++ RoutePlannerLib/Util/SimpleObjectWriter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
 {
@@ -31,14 +33,53 @@ Hinweise:
 
         public SimpleObjectWriter(TextWriter stream)
         {
-            Stream = stream;
+            Stream = stream;         
+        }
+        long fakultaet_rek(long n)
+        { 
+        if(n==1){
+            return 1;
+        }
+        else{ // Rekursionsfall
+        long ergebnis = n * fakultaet_rek(n - 1); return ergebnis;
+        }
         }
 
-
-        public void Next(object c1)
+public void Next(object c1)
         {
-            throw new NotImplementedException();
-        }
+            if(c1 != null)
+            {
+                Type myType = c1.GetType();
+                var obj = c1;
+                var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+                var newStream = Stream;
+                var i = 0;
+                Type type = assemblies[0].GetType(myType.FullName); ;
+                while (i < assemblies.Length && type != null)
+                {
+                    type = assemblies[i].GetType(myType.FullName); // type of assembly
+                }
+                Stream.WriteLine($"Instance of {{type}}\r\n");
+                
 
+                IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+
+                foreach (PropertyInfo prop in props)
+                {
+                    object propValue = prop.GetValue(c1, null);
+                    //if (propValue.GetType())
+                    //{
+
+                        newStream.WriteLine("Location is a nested object...\r\n");
+                        Next(propValue);
+                    //}
+                    //else
+                    //{
+                        newStream.WriteLine(prop.Name+"="+propValue+"\r\n");
+                    //}
+                    // Do something with propValue
+                }
+            }
+        }
     }
 }
